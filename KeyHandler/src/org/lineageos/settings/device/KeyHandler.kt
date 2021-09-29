ab941226533dd0eae102d20ae85c1fa67a3baddd
Timi Rautamäki <timi.rautamaki@gmail.com>
2021-09-29 19:47:17 +0000

    KeyHandler: configurable modes
    Change-Id: I95def088653a70428505336d996ba86ff7d3f29a

--- KeyHandler/src/org/lineageos/settings/device/KeyHandler.kt
+++ KeyHandler/src/org/lineageos/settings/device/KeyHandler.kt
@@ -5,30 +5,39 @@
 
 package org.lineageos.settings.device
 
+import android.content.ContentResolver
 import android.content.Context
+import android.database.ContentObserver
 import android.media.AudioManager
+import android.os.Handler
+import android.os.Looper
+import android.os.UserHandle
 import android.os.VibrationEffect
 import android.os.Vibrator
 import android.view.KeyEvent
 import com.android.internal.os.DeviceKeyHandler
 
-class KeyHandler(context: Context) : DeviceKeyHandler {
+import lineageos.providers.LineageSettings
+
+class KeyHandler(private val context: Context) : DeviceKeyHandler {
     private val audioManager = context.getSystemService(AudioManager::class.java)
     private val vibrator = context.getSystemService(Vibrator::class.java)
+
+    val RINGER_MODES: IntArray;
 
     override fun handleKeyEvent(event: KeyEvent): KeyEvent {
         if (event.action == KeyEvent.ACTION_DOWN) {
             when (event.scanCode) {
-                MODE_NORMAL -> {
-                    audioManager.setRingerModeInternal(AudioManager.RINGER_MODE_NORMAL)
+                POS_TOP -> {
+                    audioManager.setRingerModeInternal(RINGER_MODES[0])
                     vibrator.vibrate(MODE_NORMAL_EFFECT)
                 }
-                MODE_VIBRATION -> {
-                    audioManager.setRingerModeInternal(AudioManager.RINGER_MODE_VIBRATE)
+                POS_MIDDLE -> {
+                    audioManager.setRingerModeInternal(RINGER_MODES[1])
                     vibrator.vibrate(MODE_VIBRATION_EFFECT)
                 }
-                MODE_SILENCE -> {
-                    audioManager.setRingerModeInternal(AudioManager.RINGER_MODE_SILENT)
+                POS_BOTTOM -> {
+                    audioManager.setRingerModeInternal(RINGER_MODES[3])
                 }
             }
         }
@@ -39,12 +48,66 @@ class KeyHandler(context: Context) : DeviceKeyHandler {
         private const val TAG = "KeyHandler"
 
         // Slider key codes
-        private const val MODE_NORMAL = 601
-        private const val MODE_VIBRATION = 602
-        private const val MODE_SILENCE = 603
+        private const val POS_TOP = 601
+        private const val POS_MIDDLE = 602
+        private const val POS_BOTTOM = 603
 
         // Vibration effects
         private val MODE_NORMAL_EFFECT = VibrationEffect.get(VibrationEffect.EFFECT_HEAVY_CLICK)
         private val MODE_VIBRATION_EFFECT = VibrationEffect.get(VibrationEffect.EFFECT_DOUBLE_CLICK)
     }
+
+    init {
+        // Ringer modes
+        val resolver : ContentResolver = context.getContentResolver();
+        RINGER_MODES = intArrayOf(
+            LineageSettings.System.getInt(resolver,
+                LineageSettings.System.KEY_NOTIF_SLIDER_TOP_POS_MODE, 0),
+            LineageSettings.System.getInt(resolver,
+                LineageSettings.System.KEY_NOTIF_SLIDER_MIDDLE_POS_MODE, 0),
+            LineageSettings.System.getInt(resolver,
+                LineageSettings.System.KEY_NOTIF_SLIDER_BOTTOM_POS_MODE, 0)
+        )
+    }
+
+/*    private val settingsObserver = object : ContentObserver(Handler(Looper.getMainLooper())) {
+        fun register() {
+            getContentResolver().registerContentObserver(
+                LineageSettings.System.getUriFor(LineageSettings.System.KEY_NOTIF_SLIDER_TOP_POS_MODE),
+                false,
+                this
+            )
+            getContentResolver().registerContentObserver(
+                LineageSettings.System.getUriFor(LineageSettings.System.KEY_NOTIF_SLIDER_MIDDLE_POS_MODE),
+                false,
+                this
+            )
+            getContentResolver().registerContentObserver(
+                LineageSettings.System.getUriFor(LineageSettings.System.KEY_NOTIF_SLIDER_BOTTOM_POS_MODE),
+                false,
+                this
+            )
+
+            onChange(true)
+        }
+
+        fun unregister() {
+            contentResolver.unregisterContentObserver(this)
+        }
+
+        override fun onChange(selfChange: Boolean) {
+            RINGER_MODES[0] = LineageSettings.System.getIntForUser(contentResolver,
+                LineageSettings.System.KEY_NOTIF_SLIDER_TOP_POS_MODE, 0,
+                UserHandle.USER_CURRENT
+            )
+            RINGER_MODES[0] = LineageSettings.System.getIntForUser(contentResolver,
+                LineageSettings.System.KEY_NOTIF_SLIDER_MIDDLE_POS_MODE, 0,
+                UserHandle.USER_CURRENT
+            )
+            RINGER_MODES[0] = LineageSettings.System.getIntForUser(contentResolver,
+                LineageSettings.System.KEY_NOTIF_SLIDER_BOTTOM_POS_MODE, 0,
+                UserHandle.USER_CURRENT
+            )
+        }
+    }*/
 }
