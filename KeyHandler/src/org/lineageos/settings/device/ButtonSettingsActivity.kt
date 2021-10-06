4dee1982665d3462d0955965f07f3d9acf41e7ea
Timi Rautamäki <timi.rautamaki@gmail.com>
2021-10-06 13:17:27 +0000

    KeyHandler: add customization options
    Change-Id: I5f50f5d442342b2082bf47e11b1351e578fa0144

---
+++ KeyHandler/src/org/lineageos/settings/device/ButtonSettingsActivity.kt
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ *               2021 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.lineageos.settings.device
+
+import android.R
+import android.preference.PreferenceActivity
+import android.os.Bundle
+
+class ButtonSettingsActivity : PreferenceActivity() {
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        fragmentManager.beginTransaction().replace(
+            R.id.content,
+            ButtonSettingsFragment()
+        ).commit()
+    }
+}
