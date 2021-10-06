d83931a5b553ebda7a6270395367d46c8ec1442b
Timi Rautamäki <timi.rautamaki@gmail.com>
2021-10-06 14:19:02 +0000

    WIP KeyHandler: use shared prefs
    Change-Id: Ic27922dda8d7630f45423e73336182bcf6ff86ec

--- KeyHandler/src/org/lineageos/settings/device/ConfigPanelSearchIndexablesProvider.kt
+++ KeyHandler/src/org/lineageos/settings/device/ConfigPanelSearchIndexablesProvider.kt
@@ -1,53 +1,50 @@
 /*
  * Copyright (C) 2016 The CyanogenMod Project
- * Copyright (C) 2017 The LineageOS Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *               2021 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
  */
+
 package org.lineageos.settings.device
 
 import android.database.Cursor
+import android.database.MatrixCursor
 import android.provider.SearchIndexableResource
+import android.provider.SearchIndexablesProvider
+
+import android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_CLASS_NAME;
+import android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_ICON_RESID;
+import android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_INTENT_ACTION;
+import android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_INTENT_TARGET_CLASS;
+import android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_INTENT_TARGET_PACKAGE;
+import android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_RANK;
+import android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_RESID;
+import android.provider.SearchIndexablesContract.INDEXABLES_RAW_COLUMNS;
+import android.provider.SearchIndexablesContract.INDEXABLES_XML_RES_COLUMNS;
+import android.provider.SearchIndexablesContract.NON_INDEXABLES_KEYS_COLUMNS;
 
 class ConfigPanelSearchIndexablesProvider : SearchIndexablesProvider() {
-    fun onCreate(): Boolean {
+    override fun onCreate(): Boolean {
         return true
     }
 
-    fun queryXmlResources(projection: Array<String?>?): Cursor {
+    override fun queryXmlResources(projection: Array<String?>?): Cursor {
         val cursor = MatrixCursor(INDEXABLES_XML_RES_COLUMNS)
-        if (Startup.hasButtonProcs() /* show button panel */) {
+        //if (Startup.hasButtonProcs() /* show button panel */) {
             cursor.addRow(
                 generateResourceRef(
                     INDEXABLE_RES[SEARCH_IDX_BUTTON_PANEL]
                 )
             )
-        }
-        if (Startup.hasOClick() /* show oclick panel */) {
-            cursor.addRow(
-                generateResourceRef(
-                    INDEXABLE_RES[SEARCH_IDX_OCLICK_PANEL]
-                )
-            )
-        }
+        //}
+        
         return cursor
     }
 
-    fun queryRawData(projection: Array<String?>?): Cursor {
+    override fun queryRawData(projection: Array<String?>?): Cursor {
         return MatrixCursor(INDEXABLES_RAW_COLUMNS)
     }
 
-    fun queryNonIndexableKeys(projection: Array<String?>?): Cursor {
+    override fun queryNonIndexableKeys(projection: Array<String?>?): Cursor {
         return MatrixCursor(NON_INDEXABLES_KEYS_COLUMNS)
     }
 
@@ -62,11 +59,6 @@ class ConfigPanelSearchIndexablesProvider : SearchIndexablesProvider() {
                     1, R.xml.button_panel,
                     ButtonSettingsActivity::class.java.name,
                     R.drawable.ic_settings_additional_buttons
-                ),
-                SearchIndexableResource(
-                    1, R.xml.oclick_panel,
-                    BluetoothInputSettings::class.java.getName(),
-                    R.drawable.ic_oclick_notification
                 )
             )
 
