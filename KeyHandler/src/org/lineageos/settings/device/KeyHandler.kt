996c967fbcfbb471b92badd35e4b05fc616dfc47
Timi Rautamäki <timi.rautamaki@gmail.com>
2021-10-06 13:17:27 +0000

    KeyHandler: add customization options
    Change-Id: I5f50f5d442342b2082bf47e11b1351e578fa0144

--- KeyHandler/src/org/lineageos/settings/device/KeyHandler.kt
+++ KeyHandler/src/org/lineageos/settings/device/KeyHandler.kt
@@ -6,42 +6,76 @@
 package org.lineageos.settings.device
 
 import android.content.Context
+import android.content.SharedPreferences
 import android.media.AudioManager
 import android.os.VibrationEffect
 import android.os.Vibrator
 import android.view.KeyEvent
+import androidx.preference.PreferenceManager
+
 import com.android.internal.os.DeviceKeyHandler
 
-class KeyHandler(context: Context) : DeviceKeyHandler {
+class KeyHandler(private val context: Context) : DeviceKeyHandler {
     private val audioManager = context.getSystemService(AudioManager::class.java)
     private val vibrator = context.getSystemService(Vibrator::class.java)
+    private val packageContext = context.createPackageContext(
+            KeyHandler::class.java.getPackage().name, 0)
 
     override fun handleKeyEvent(event: KeyEvent): KeyEvent {
         if (event.action == KeyEvent.ACTION_DOWN) {
             when (event.scanCode) {
-                MODE_NORMAL -> {
-                    audioManager.setRingerModeInternal(AudioManager.RINGER_MODE_NORMAL)
-                    vibrator.vibrate(MODE_NORMAL_EFFECT)
+                POSITION_TOP -> {
+                    val mode = packageContext.getSharedPreferences(
+                            packageContext.getPackageName() + "_preferences",
+                            Context.MODE_PRIVATE or Context.MODE_MULTI_PROCESS)
+                            .getString(NOTIF_SLIDER_TOP_KEY, "2").toInt()
+                    audioManager.setRingerModeInternal(mode)
+                    vibrateIfNeeded(mode)
                 }
-                MODE_VIBRATION -> {
-                    audioManager.setRingerModeInternal(AudioManager.RINGER_MODE_VIBRATE)
-                    vibrator.vibrate(MODE_VIBRATION_EFFECT)
+                POSITION_MIDDLE -> {
+                    val mode = packageContext.getSharedPreferences(
+                            packageContext.getPackageName() + "_preferences",
+                            Context.MODE_PRIVATE or Context.MODE_MULTI_PROCESS)
+                            .getString(NOTIF_SLIDER_MIDDLE_KEY, "1").toInt()
+                    audioManager.setRingerModeInternal(mode)
+                    vibrateIfNeeded(mode)
                 }
-                MODE_SILENCE -> {
-                    audioManager.setRingerModeInternal(AudioManager.RINGER_MODE_SILENT)
+                POSITION_BOTTOM -> {
+                    val mode = packageContext.getSharedPreferences(
+                            packageContext.getPackageName() + "_preferences",
+                            Context.MODE_PRIVATE or Context.MODE_MULTI_PROCESS)
+                            .getString(NOTIF_SLIDER_BOTTOM_KEY, "0").toInt()
+                    audioManager.setRingerModeInternal(mode)
+                    vibrateIfNeeded(mode)
                 }
             }
         }
         return event
     }
 
+    fun vibrateIfNeeded(mode: Int) {
+        when (mode) {
+            AudioManager.RINGER_MODE_VIBRATE -> {
+                vibrator.vibrate(MODE_VIBRATION_EFFECT)
+            }
+            AudioManager.RINGER_MODE_NORMAL -> {
+                vibrator.vibrate(MODE_NORMAL_EFFECT)
+            }
+        }
+    }
+
     companion object {
         private const val TAG = "KeyHandler"
 
         // Slider key codes
-        private const val MODE_NORMAL = 601
-        private const val MODE_VIBRATION = 602
-        private const val MODE_SILENCE = 603
+        private const val POSITION_TOP = 601
+        private const val POSITION_MIDDLE = 602
+        private const val POSITION_BOTTOM = 603
+
+        // Preference keys
+        private const val NOTIF_SLIDER_TOP_KEY = "config_top_position"
+        private const val NOTIF_SLIDER_MIDDLE_KEY = "config_middle_position"
+        private const val NOTIF_SLIDER_BOTTOM_KEY = "config_bottom_position"
 
         // Vibration effects
         private val MODE_NORMAL_EFFECT = VibrationEffect.get(VibrationEffect.EFFECT_HEAVY_CLICK)
