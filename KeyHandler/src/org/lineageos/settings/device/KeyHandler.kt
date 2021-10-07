b46b6ab53ad19d332c412eb515f776d1785f929c
Timi Rautamäki <timi.rautamaki@gmail.com>
2021-10-07 18:12:16 +0000

    KeyHandler: Add more modes
    * Add DND modes
     * alarms only
     * important interruptions
     * total silence
    
    * Add option to mute media when going to silent mode
    
    Change-Id: Ia59e06f0a0dd3047b3771bb3ac8d5e338d0a75c3

--- KeyHandler/src/org/lineageos/settings/device/KeyHandler.kt
+++ KeyHandler/src/org/lineageos/settings/device/KeyHandler.kt
@@ -5,46 +5,68 @@
 
 package org.lineageos.settings.device
 
+import android.app.NotificationManager
+import android.content.BroadcastReceiver
 import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.media.AudioManager
 import android.os.VibrationEffect
 import android.os.Vibrator
+import android.provider.Settings
 import android.view.KeyEvent
 import com.android.internal.os.DeviceKeyHandler
 
 class KeyHandler(context: Context) : DeviceKeyHandler {
     private val audioManager = context.getSystemService(AudioManager::class.java)
+    private val notificationManager = context.getSystemService(NotificationManager::class.java);
     private val vibrator = context.getSystemService(Vibrator::class.java)
     private val packageContext = context.createPackageContext(
         KeyHandler::class.java.getPackage()!!.name, 0
     )
 
+    private var wasMuted = false
+    private val broadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            if (intent?.getBooleanExtra(AudioManager.EXTRA_STREAM_VOLUME_MUTED, false) == false) {
+                wasMuted = false
+            }
+        }
+    }
+
+    init {
+        context.registerReceiver(
+            broadcastReceiver,
+            IntentFilter(AudioManager.STREAM_MUTE_CHANGED_ACTION)
+        )
+    }
+
     override fun handleKeyEvent(event: KeyEvent): KeyEvent {
         if (event.action == KeyEvent.ACTION_DOWN) {
             when (event.scanCode) {
                 POSITION_TOP -> {
-                    val mode = packageContext.getSharedPreferences(
-                        packageContext.packageName + "_preferences",
-                        Context.MODE_PRIVATE or Context.MODE_MULTI_PROCESS
-                    ).getString(NOTIF_SLIDER_TOP_KEY, "2")!!.toInt()
-                    audioManager.setRingerModeInternal(mode)
-                    vibrateIfNeeded(mode)
+                    handleMode(
+                        packageContext.getSharedPreferences(
+                            packageContext.packageName + "_preferences",
+                            Context.MODE_PRIVATE or Context.MODE_MULTI_PROCESS
+                        ).getString(NOTIF_SLIDER_TOP_KEY, "2")!!.toInt()
+                    )
                 }
                 POSITION_MIDDLE -> {
-                    val mode = packageContext.getSharedPreferences(
-                        packageContext.packageName + "_preferences",
-                        Context.MODE_PRIVATE or Context.MODE_MULTI_PROCESS
-                    ).getString(NOTIF_SLIDER_MIDDLE_KEY, "1")!!.toInt()
-                    audioManager.setRingerModeInternal(mode)
-                    vibrateIfNeeded(mode)
+                    handleMode(
+                        packageContext.getSharedPreferences(
+                            packageContext.packageName + "_preferences",
+                            Context.MODE_PRIVATE or Context.MODE_MULTI_PROCESS
+                        ).getString(NOTIF_SLIDER_MIDDLE_KEY, "1")!!.toInt()
+                    )
                 }
                 POSITION_BOTTOM -> {
-                    val mode = packageContext.getSharedPreferences(
-                        packageContext.packageName + "_preferences",
-                        Context.MODE_PRIVATE or Context.MODE_MULTI_PROCESS
-                    ).getString(NOTIF_SLIDER_BOTTOM_KEY, "0")!!.toInt()
-                    audioManager.setRingerModeInternal(mode)
-                    vibrateIfNeeded(mode)
+                    handleMode(
+                        packageContext.getSharedPreferences(
+                            packageContext.packageName + "_preferences",
+                            Context.MODE_PRIVATE or Context.MODE_MULTI_PROCESS
+                        ).getString(NOTIF_SLIDER_BOTTOM_KEY, "0")!!.toInt()
+                    )
                 }
             }
         }
@@ -62,6 +84,35 @@ class KeyHandler(context: Context) : DeviceKeyHandler {
         }
     }
 
+    private fun handleMode(mode: Int) {
+        val muteMedia = packageContext.getSharedPreferences(
+            packageContext.packageName + "_preferences",
+            Context.MODE_PRIVATE or Context.MODE_MULTI_PROCESS
+        ).getBoolean(MUTE_MEDIA_WITH_SILENT, false)
+
+        when (mode) {
+            AudioManager.RINGER_MODE_SILENT -> {
+                notificationManager.setZenMode(Settings.Global.ZEN_MODE_OFF, null, TAG)
+                audioManager.setRingerModeInternal(mode)
+                if (muteMedia) {
+                    audioManager.adjustVolume(AudioManager.ADJUST_MUTE, 0)
+                    wasMuted = true
+                }
+            }
+            AudioManager.RINGER_MODE_VIBRATE, AudioManager.RINGER_MODE_NORMAL -> {
+                notificationManager.setZenMode(Settings.Global.ZEN_MODE_OFF, null, TAG)
+                audioManager.setRingerModeInternal(mode)
+                if (muteMedia && wasMuted) audioManager.adjustVolume(AudioManager.ADJUST_UNMUTE, 0)
+            }
+            ZEN_PRIORITY_ONLY, ZEN_TOTAL_SILENCE, ZEN_ALARMS_ONLY -> {
+                audioManager.setRingerModeInternal(AudioManager.RINGER_MODE_NORMAL);
+                notificationManager.setZenMode(mode - ZEN_OFFSET, null, TAG)
+                if (muteMedia && wasMuted) audioManager.adjustVolume(AudioManager.ADJUST_UNMUTE, 0)
+            }
+        }
+        vibrateIfNeeded(mode)
+    }
+
     companion object {
         private const val TAG = "KeyHandler"
 
@@ -74,6 +125,13 @@ class KeyHandler(context: Context) : DeviceKeyHandler {
         private const val NOTIF_SLIDER_TOP_KEY = "config_top_position"
         private const val NOTIF_SLIDER_MIDDLE_KEY = "config_middle_position"
         private const val NOTIF_SLIDER_BOTTOM_KEY = "config_bottom_position"
+        private const val MUTE_MEDIA_WITH_SILENT = "config_mute_media"
+
+        // ZEN constants
+        private const val ZEN_OFFSET = 2
+        private const val ZEN_PRIORITY_ONLY = 3
+        private const val ZEN_TOTAL_SILENCE = 4
+        private const val ZEN_ALARMS_ONLY = 5
 
         // Vibration effects
         private val MODE_NORMAL_EFFECT = VibrationEffect.get(VibrationEffect.EFFECT_HEAVY_CLICK)
