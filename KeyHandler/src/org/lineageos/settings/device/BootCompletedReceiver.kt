34ef11ebfc421e034745db7bb06593fe5a2e38d8
Timi Rautamäki <timi.rautamaki@gmail.com>
2021-10-19 13:08:14 +0000

    KeyHandler: use UEventObserver
    Change-Id: I2e6c5d2861569750bead05edacf6e328a5227077

---
+++ KeyHandler/src/org/lineageos/settings/device/BootCompletedReceiver.kt
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2021 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.lineageos.settings.device
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+
+class BootCompletedReceiver : BroadcastReceiver() {
+    private var TAG = "KeyHandler"
+
+    override fun onReceive(context: Context, intent: Intent?) {
+        Log.d(TAG, "Starting")
+        context.startService(Intent(context, KeyHandler::class.java))
+    }
+}
