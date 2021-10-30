cb4b55fabfed538252c6b7b6834d4a4afa41a292
TheScarastic <warabhishek@gmail.com>
2021-10-30 08:30:42 +0200

    hidl: fingerprint: Uprev to 2.3
     * Move fingerprint HIDL to 2.3
     * Remove Lineage FOD HIDL
     * Rename fodextension to udfpsextension
    
    Change-Id: I8c628d2ee9158b45eeb04dd324b56ef3697296b7

--- hidl/fingerprint/UdfpsExtension.cpp
+++ hidl/fingerprint/UdfpsExtension.cpp
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <compositionengine/FodExtension.h>
+#include <compositionengine/UdfpsExtension.h>
 
 #if __has_include(<display/drm/sde_drm.h>)
 #include <display/drm/sde_drm.h>
@@ -11,7 +11,7 @@
 #include <drm/sde_drm.h>
 #endif
 
-uint32_t getFodZOrder(uint32_t z, bool touched) {
+uint32_t getUdfpsZOrder(uint32_t z, bool touched) {
 #ifdef FOD_PRESSED_LAYER_ZORDER
     return touched ? z | FOD_PRESSED_LAYER_ZORDER : z;
 #else
@@ -19,6 +19,6 @@ uint32_t getFodZOrder(uint32_t z, bool touched) {
 #endif
 }
 
-uint64_t getFodUsageBits(uint64_t usageBits, bool) {
+uint64_t getUdfpsUsageBits(uint64_t usageBits, bool) {
     return usageBits;
 }
