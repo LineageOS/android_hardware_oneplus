4d4de03b4689bf14fdaf77e75e14ce444ebe3fe9
alk3pInjection <webmaster@raspii.tech>
2022-01-22 12:34:45 +0800

    hidl: fingerprint: Pretend to enable dim on auth()
     * but actually not .^)
    
       In fact, this simulates the old behavior of los fod.
    
    Change-Id: Ia7bba1d2e58bf46d66b3a424a1fc233299acc98b

--- hidl/fingerprint/BiometricsFingerprint.cpp
+++ hidl/fingerprint/BiometricsFingerprint.cpp
@@ -34,7 +34,10 @@
 #define OP_DISPLAY_NOTIFY_PRESS 9
 #define OP_DISPLAY_SET_DIM 10
 
+#define AUTH_STATUS_PATH  "/sys/class/drm/card0-DSI-1/auth_status"
 #define POWER_STATUS_PATH "/sys/class/drm/card0-DSI-1/power_status"
+
+int isCancelled = 0;
 
 namespace {
 
@@ -101,6 +104,7 @@ Return<void> BiometricsFingerprint::onFingerDown(uint32_t, uint32_t, float, floa
 }
 
 Return<void> BiometricsFingerprint::onFingerUp() {
+    isCancelled = 0;
     mVendorDisplayService->setMode(OP_DISPLAY_NOTIFY_PRESS, 0);
 
     return Void();
@@ -225,6 +229,7 @@ Return<uint64_t> BiometricsFingerprint::getAuthenticatorId() {
 }
 
 Return<RequestStatus> BiometricsFingerprint::cancel() {
+    isCancelled = 1;
     mVendorDisplayService->setMode(OP_DISPLAY_SET_DIM, 0);
     mVendorFpService->updateStatus(OP_FINISH_FP_ENROLL);
     mVendorFpService->updateStatus(OP_ENABLE_FP_LONGPRESS);
@@ -257,7 +262,10 @@ Return<RequestStatus> BiometricsFingerprint::setActiveGroup(uint32_t gid,
 Return<RequestStatus> BiometricsFingerprint::authenticate(uint64_t operationId,
         uint32_t gid) {
     set(POWER_STATUS_PATH, 1);
-    mVendorDisplayService->setMode(OP_DISPLAY_SET_DIM, 1);
+    if (isCancelled)
+        mVendorDisplayService->setMode(OP_DISPLAY_SET_DIM, 1);
+    else
+        set(AUTH_STATUS_PATH, 1);
     mVendorFpService->updateStatus(OP_ENABLE_FP_LONGPRESS);
 
     return ErrorFilter(mDevice->authenticate(mDevice, operationId, gid));
