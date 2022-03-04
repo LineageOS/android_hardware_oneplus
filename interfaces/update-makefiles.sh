2c9362ea9365ebbf4d100d53330e3d3f9a30d6bb
Bruno Martins <bgcngm@gmail.com>
2022-03-04 16:38:44 +0000

    interfaces: Split between vendors
    This should make update-makefiles.sh usable.
    
    Change-Id: I0e10968292464eac49c6b741bf277201962fdaa3

--- interfaces/update-makefiles.sh
+++ interfaces/update-makefiles.sh
@@ -3,4 +3,7 @@
 source $ANDROID_BUILD_TOP/system/tools/hidl/update-makefiles-helper.sh
 
 do_makefiles_update \
-  "vendor.oneplus:hardware/oneplus/interfaces"
+  "vendor.goodix:hardware/oneplus/interfaces/goodix"
+
+do_makefiles_update \
+  "vendor.oneplus:hardware/oneplus/interfaces/oneplus"
