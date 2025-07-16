Oneplus-Nord5-development/android_hardware_oplus

touch: Use CHECK_EQ(a, b) assertion
Change-Id: I7522c86f168a2a5226b2ef28903afa74cee62997

aidl/touch/service.cpp
@@ -26,18 +26,14 @@ int main() {
         const std::string instance = std::string(HighTouchPollingRate::descriptor) + "/default";
         const binder_status_t status =
                 AServiceManager_addService(htpr->asBinder().get(), instance.c_str());
-        if (status != STATUS_OK) {
-            LOG(WARNING) << "Can't register IHighTouchPollingRate/default";
-        }
+        CHECK_EQ(status, STATUS_OK) << "Failed to add service " << instance << " " << status;
     }
 
     if (tg) {
         const std::string instance = std::string(TouchscreenGesture::descriptor) + "/default";
         const binder_status_t status =
                 AServiceManager_addService(tg->asBinder().get(), instance.c_str());
-        if (status != STATUS_OK) {
-            LOG(WARNING) << "Can't register ITouchscreenGesture/default";
-        }
+        CHECK_EQ(status, STATUS_OK) << "Failed to add service " << instance << " " << status;
     }
 
     ABinderProcess_joinThreadPool();

LineageOS/android_hardware_oneplus

aidl/touch/service.cpp
@@ -21,9 +21,7 @@ int main() {
 
     const std::string instanceTg = std::string(TouchscreenGesture::descriptor) + "/default";
     status = AServiceManager_addService(tg->asBinder().get(), instanceTg.c_str());
-    if (status != STATUS_OK) {
-        LOG(WARNING) << "Can't register ITouchscreenGesture/default";
-    }
+    CHECK_EQ(status, STATUS_OK) << "Failed to add service " << instanceTg << " " << status;
 
     ABinderProcess_joinThreadPool();
     return EXIT_FAILURE;  // should not reach