6ab608ce3f0cc0bd8661be6f9d5bbabda65edd47
LuK1337 <priv.luk@gmail.com>
2025-08-15 10:12:25 +0200

    livedisplay: Migrate to AIDL
    Change-Id: I8dbe9ed99549aff3e7fcff553482a4c45aae67ef

---
+++ aidl/livedisplay/include/livedisplay/oneplus/AntiFlicker.h
@@ -0,0 +1,24 @@
+/*
+ * SPDX-FileCopyrightText: 2021-2025 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#include <aidl/vendor/lineage/livedisplay/BnAntiFlicker.h>
+
+namespace aidl {
+namespace vendor {
+namespace lineage {
+namespace livedisplay {
+
+class AntiFlicker : public BnAntiFlicker {
+    // Methods from ::aidl::vendor::lineage::livedisplay::BnAntiFlicker follow.
+    ndk::ScopedAStatus getEnabled(bool* _aidl_return) override;
+    ndk::ScopedAStatus setEnabled(bool enabled) override;
+};
+
+}  // namespace livedisplay
+}  // namespace lineage
+}  // namespace vendor
+}  // namespace aidl
