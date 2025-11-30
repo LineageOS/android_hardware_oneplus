93a56a48ae16aacf29757711725bcb2434fea2c7
LuK1337 <priv.luk@gmail.com>
2025-11-30 16:18:46 +0100

    doze: Fix ClassCastException on pickup pref change
    Fixes: https://gitlab.com/LineageOS/issues/android/-/issues/9633
    Change-Id: I6662f7a11fd7aae4df86e534b1900ea1b04281f5

--- doze/src/org/lineageos/settings/doze/DozeSettingsFragment.kt
+++ doze/src/org/lineageos/settings/doze/DozeSettingsFragment.kt
@@ -80,12 +80,13 @@ class DozeSettingsFragment : PreferenceFragmentCompat(), Preference.OnPreference
     }
 
     override fun onPreferenceChange(preference: Preference, newValue: Any?): Boolean {
-        val isChecked = newValue as Boolean
         when (preference.key) {
             Utils.ALWAYS_ON_DISPLAY -> {
+                val isChecked = newValue as Boolean
                 Utils.enableAlwaysOn(requireContext(), isChecked)
             }
             Utils.DOZE_ENABLE -> {
+                val isChecked = newValue as Boolean
                 Utils.enableDoze(requireContext(), isChecked)
                 Utils.checkDozeService(requireContext())
 
